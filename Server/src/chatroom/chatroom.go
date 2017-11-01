e41840ef1542d29209252f7b8e1cf7c39fa6b309
yimk <kipanyim@gmail.com>
2017-11-01 20:00:56 +0000

    chang slice to map

--- Server/src/chatroom/chatroom.go
+++ Server/src/chatroom/chatroom.go
@@ -11,17 +11,22 @@ var JOIN_CHATROOM_RESPONSE_PROTOCOL = [5]string{"JOINED_CHATROOM", "SERVER_IP", 
 var SEND_MESSAGE_REPSONSE_PROTOCOL = [3]string{"CHAT", "CLIENT_NAME", "MESSAGE"}
 var LEAVE_CHATROOM_RESPONSE_PROTOCOL = [2]string{"LEFT_CHATROOM", "JOIN_ID"}
 
-// list of all chatrooms , each chatroom's index in the list is it's reference
-var chatRooms []string
 
-// list of all users names, it's index is this user's join id
-var usersName []string
+type Groups struct {
+        groups []int
+    }
+
+// list of all chatrooms , each chatroom's index in the list is it's reference
+var chatRooms map[string]int
+
+// map of all users names, it's index is this user's join id
+var usersName map[string]int
 
 // list of all users' corresponding group,
-var usersCorrespondingGroup [][]int
+var usersCorrespondingGroup map[int]Groups
 
 // list of users' connection e.g users_conns[0].send(message)
-var usersConns []net.Conn
+var usersConns map[int]net.Conn
 
 
 func RequestJoinChatroom(request string, clientConn net.Conn, port string) bool{
@@ -96,12 +101,12 @@ func RequestDisconnect(request string, clientConn net.Conn, serverPort string) b
 	// Parse request for essential information
 	requestLines := strings.Split(request, "\n")
 	clientName := strings.Split(requestLines[2], ":")[1]
-	joinId := findUserIndex(clientName)
+	joinId := userName[clientName]
 
 	fmt.Printf("Disconnection:")
 	fmt.Printf("Client:", clientName)
 	fmt.Printf("ID:", joinId)
-	fmt.Printf("Room:", usersCorrespondingGroup[joinId])
+	// fmt.Printf("Room:", usersCorrespondingGroup[joinId])
 
 
 	for _, roomRef := range usersCorrespondingGroup[joinId]{
@@ -128,20 +133,20 @@ func createChatroom(roomName string) {
 	chatRooms = append(chatRooms, roomName)
 }
 
-func joinChatroom(roomName string, userName string, clientConn net.Conn) (string, string){
+func joinChatroom(roomName string, usersName string, clientConn net.Conn) (string, string){
 
 	//get the ref of the chat room
-	ref := findChatroomIndex(roomName)
+	ref := chatRoom[roomName]
 
 	//add new user
 	fmt.Println("User Conn:" + clientConn.LocalAddr().String() + "\n")
-	joinId := findUserConnIndex(clientConn)
+	joinId := usersName[usersName]
 
-	if (joinId != -1) {
+	if (val, conn := usersConns[joinId]; conn) {
 
 		// if user connection existis, then simply add new group to users chatroom record
 		fmt.Printf("Client Conn Exists")
-		usersCorrespondingGroup[joinId] = append(usersCorrespondingGroup[joinId], ref)
+		usersCorrespondingGroup[joinId].groups = append(usersCorrespondingGroup[joinId].groups, ref)
 
 	} else if (findUserIndex(userName) != -1) {
 
@@ -168,13 +173,14 @@ func joinChatroom(roomName string, userName string, clientConn net.Conn) (string
 func broadCastWithinRoom(message string, roomRef string) {
 
 	roomRefInt, _ :=  strconv.Atoi(roomRef)
-	fmt.Printf("Room: %d\n", roomRef )
+	fmt.Printf("\nBroadCast Room: %d\n", roomRef )
 
 	for index, conn := range usersConns {
 		fmt.Printf("JOIN ID: ", index )
 		fmt.Printf("Client: ", usersName[index] )
-		//fmt.Printf( usersCorrespondingGroup[index],"\n")
+
 		for _, group := range usersCorrespondingGroup[index] {
+			
 			if (roomRefInt == group) {
 				conn.Write([]byte(message))
 				break
